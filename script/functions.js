c6ecfcbc372a45c0f4784decb46f9d426bf40532
Alessandro <andro.mieli2001@gmail.com>
2020-04-20 21:13:12 +0200

    Removing cookie
    they'r useless for now

--- script/functions.js
+++ script/functions.js
@@ -1,27 +1,17 @@
 /* -------------------- Variables -------------------- */
-var alba;
-var tramonto; 
-var darkMode = false;
 var pc = "(min-width: 993px)";
 var largeTablet = "(max-width: 992px)";
 var tablet = "(max-width: 768px)";
 var smartphone = "(max-width: 576px)";
 var smallSmartphone = "(max-width: 343px)";
 
-/* -------------------- Daylight or Nighttime -------------------- */
-// (Temporarily Discontinued)
-function checkTime() {
-    currentDate = Math.round(new Date().getTime()/1000);
-    
-    if (currentDate >= alba || currentDate <= tramonto) {
-        // Daylight
-        return true;
-    }
-    else {
-        // Nighttime
-        return false;
-    }
-}
+/* -------------------- Window on load -------------------- */
+window.onload = function () {
+    $.getJSON("https://ipinfo.io/geo", function (data) {
+        // Get country name -> IT
+        changeLanSite(data.country);
+    });
+};
 
 /* -------------------- Open/Close navbar on mobile -------------------- */
 function openNav() {
@@ -34,63 +24,45 @@ function closeNav() {
 }
 
 /* -------------------- Change color on contact link hover -------------------- */
-$(document).ready(function() {
-    $('#contact-item-a').hover(function(){     
+$(document).ready(function () {
+    $('#contact-item-a').hover(function () {
         $('#contact-item-b').toggleClass('contact-item-hover');
         $('#contact-item-c').toggleClass('contact-item-hover');
     }),
-    $('#contact-item-b').hover(function(){
-        $('#contact-item-a').toggleClass('contact-item-hover');
-        $('#contact-item-c').toggleClass('contact-item-hover');
-    }),
-    $('#contact-item-c').hover(function(){
-        $('#contact-item-a').toggleClass('contact-item-hover');
-        $('#contact-item-b').toggleClass('contact-item-hover');
-    });
+        $('#contact-item-b').hover(function () {
+            $('#contact-item-a').toggleClass('contact-item-hover');
+            $('#contact-item-c').toggleClass('contact-item-hover');
+        }),
+        $('#contact-item-c').hover(function () {
+            $('#contact-item-a').toggleClass('contact-item-hover');
+            $('#contact-item-b').toggleClass('contact-item-hover');
+        });
 });
 
 /* -------------------- Scroll to Top Button -------------------- */
-$(window).scroll(function() {
+$(window).scroll(function () {
     // If page is scrolled more than 50px
-    if ($(this).scrollTop() >= 50) { 
+    if ($(this).scrollTop() >= 50) {
         // Fade in the arrow
-        $('#return-to-top').fadeIn(200);    
+        $('#return-to-top').fadeIn(200);
     } else {
         // Else fade out the arrow
-        $('#return-to-top').fadeOut(200);   
+        $('#return-to-top').fadeOut(200);
     }
 });
 // When arrow/nav-title is clicked
-$("#return-to-top,#return-to-top-mobile").click(function(){
+$("#return-to-top,#return-to-top-mobile").click(function () {
     // Scroll to top of body
     $('body,html').animate({
-        scrollTop : 0
+        scrollTop: 0
     }, 500);
 });
 
 /* -------------------- Scroll to contact me -------------------- */
-$('#contactme-link').click(function() {
+$('#contactme-link').click(function () {
     var el = document.querySelector("#contactme-section");
     // scroll to element
     el.scrollIntoView({ behavior: 'smooth' });
 });
 
-/* -------------------- Cookies -------------------- */
-function setCookie(cName, language) {
-    document.cookie = cName + "=" + language + ";path=/";
-}
-function getCookie(cName) {
-    var name = cName + "=";
-    var ca = document.cookie.split(';');
-    
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {            
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {            
-            return c.substring(name.length, c.length);
-        }
-    }
-    return false;
-}
+
